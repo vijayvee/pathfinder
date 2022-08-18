fd45b127b1990a5ccaa2df31b61690fc98caf5cf
Vijay <vijayvee@gmail.com>
2022-08-18 00:08:43 +0000

    Model runners

---
+++ run_models_5ep_hgru.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+seeds=(10 56 1234)
+for i in {1..3}
+do
+	python train.py -cfg hgru -nl 3 --epochs 5 -b 256 --lr 1e-4 --rank 0 --world-size 1 \
+	       	--gpu $1 /home/vveeraba/src/pathfinder_full/curv_contour_length_14 -wd 0 --in-res 160 \
+	       	--expname pf14_hgru_ts12_fsize9_k32 --seed ${seeds[$i-1]}
+done
