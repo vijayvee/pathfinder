0602395717243aef0306872cd1243749dc8a4c06
Vijay <vijayvee@gmail.com>
2022-08-20 21:24:33 +0000

    Model runners and config files changes

--- run_models_5ep.sh
+++ run_models_5ep.sh
@@ -2,7 +2,7 @@
 seeds=(10 56 1234)
 for i in {1..3}
 do
-	python train.py -cfg dalernn-t-12 -nl 3 --epochs 5 -b 256 --lr 1e-4 --rank 0 --world-size 1 \
+	python train.py -cfg dalernn-t-12-ortho -nl 3 --epochs 5 -b 256 --lr 1e-4 --rank 0 --world-size 1 \
 	       	--gpu $1 /home/vveeraba/src/pathfinder_full/curv_contour_length_14 -wd 0 --in-res 160 \
-	       	--expname pf14_dalernn_ts12_fsize9_k32 --seed ${seeds[$i-1]}
+	       	--expname pf14_dalernn_orthoinit_ts12_fsize9_k32 --seed ${seeds[$i-1]}
 done
--- run_models_5ep_hgru.sh
+++ run_models_5ep_hgru.sh
@@ -4,5 +4,5 @@ for i in {1..3}
 do
 	python train.py -cfg hgru -nl 3 --epochs 5 -b 256 --lr 1e-4 --rank 0 --world-size 1 \
 	       	--gpu $1 /home/vveeraba/src/pathfinder_full/curv_contour_length_14 -wd 0 --in-res 160 \
-	       	--expname pf14_hgru_ts12_fsize9_k32 --seed ${seeds[$i-1]}
+	       	--expname pf9_hgru_ts12_fsize9_k38 --seed ${seeds[$i-1]}
 done
