c19182e742a97932eb025e91142221f068637f3a
Vijay Veerabadran <vijayvee@gmail.com>
2022-10-03 10:22:39 -0700

    sub

--- run_models_5ep_resnet.sh
+++ run_models_5ep_resnet.sh
@@ -3,6 +3,6 @@ seeds=(10 56 1234)
 for i in {1..3}
 do
 	python train.py -cfg resnet -nl 18 --epochs 5 -b 256 --lr 1e-4 --rank 0 --world-size 1 \
-	       	--gpu $1 /home/vveeraba/src/pathfinder_full/curv_contour_length_14 -wd 0 --in-res 160 \
+	       	--gpu $1 $2 -wd 0 --in-res 160 \
 	       	--expname pf14_resnet_full --seed ${seeds[$i-1]}
 done
